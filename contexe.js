ea18fdeed71f1e93838b3188695b317bd4ad49ca
Gordon Brander <marlfoskr@gmail.com>
2011-05-17 12:50:31 -0600

    Fixed race condition.

--- contexe.js
+++ contexe.js
@@ -1,28 +1,27 @@
 !function (context, doc) {
-	var C = function (element) {
+	var Exe = function (element) {
 		var tmpEl,
 			el = element || 'html';
-		
-		if (typeof this !== C) {
-			return new C(element);
-		};
 		
 		if (el === 'html') {
 			tmpEl = doc.getElementsByTagName('html')[0];
 		}
+		/* Body will only work if the body tag has already loaded. Use at bottom of page
+		or with a DOMReady utility */
 		else if (el === 'body') {
 			tmpEl = doc.getElementsByTagName('body')[0];
+		}
+		/* Use your own DOM fragment */
+		else {
+			tmpEl = element;
 		};
 		
 		this.el = tmpEl;
-		
-		return this;
 	};
-	C.prototype.hasClass = function (classname, element) {
+	Exe.prototype.hasClass = function (classname, element) {
 		var el = element || this.el,
 			classStr = el.className,
 			classes = classStr.split(' ');
-			
 		for (var i = classes.length - 1; i >= 0; i--){
 			if (classes[i] === classname) {
 				return true;
@@ -30,12 +29,13 @@
 		};
 		return false;
 	};
-	C.prototype.given = function (classname, callback) {
+	Exe.prototype.given = function (classname, callback) {
 		if (this.hasClass(classname)) {
-			Object.call(callback);
+			callback();
 		};
+		return this;
 	};
 	
-	context['Contexe'] = C;
+	context['Contexe'] = Exe;
 	return context;
 }(this, document);
